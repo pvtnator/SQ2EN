45e9cc24dd8e13bed6cda8275ed1aee9deb80935
denhonator <denhonator@gmail.com>
2025-03-12 01:10:57 +0200

    Night book, Black Thafi fixes

--- patch.bat
+++ patch.bat
@@ -1,3 +1,4 @@
+xcopy ".\PrePatch\Data" "..\Succubus and the Blank Book\Data" /E /H /C /I /Y
 "../rpgmt_cli_v4.5/rpgmt.exe" "..\Succubus and the Blank Book"
 python patcher.py apply
 python mapConditionReplace.py
